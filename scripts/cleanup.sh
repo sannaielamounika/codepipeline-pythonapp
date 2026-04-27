a62ec22ce05ed6b8f51ec04fe989cce08b35d660
mounika <sannaielamounika12@gmail.com>
2026-04-27 21:31:19 +0000

    final fix cleanup + install

---
+++ scripts/cleanup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -ex
+
+echo "Cleaning old files..."
+
+rm -rf /home/ec2-user/app/*
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
-set -e
+set -ex
 
 APP_DIR=/home/ec2-user/app
 
+echo "Checking app directory"
+ls -l $APP_DIR
+
 cd $APP_DIR
 
-echo "Current directory:"
-pwd
-ls -l   # 👈 DEBUG
-
+# Fix: remove python3-venv (not supported)
 sudo yum update -y
-sudo yum install python3 python3-venv -y
+sudo yum install python3 -y
 
+# Create venv
 python3 -m venv venv
 source venv/bin/activate
 
-python3 -m pip install --upgrade pip
+pip install --upgrade pip
 
-# safer install
+# Install dependencies
 if [ -f requirements.txt ]; then
     pip install -r requirements.txt
 else
